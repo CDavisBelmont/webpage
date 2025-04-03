43e48865704aff194e0b197bc77cb3d80cbec792
CDavisBelmont <106823513+CDavisBelmont@users.noreply.github.com>
2025-04-03 16:58:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     fetch("collection.json")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
+        .then(response => response.json())
         .then(data => {
             const container = document.getElementById("collection");
             container.innerHTML = ""; // Clear previous content
+            
             data.forEach(item => {
                 let div = document.createElement("div");
                 div.classList.add("collection-item");
                 div.innerHTML = `
                     <h2>${item.title}</h2>
                     <p>${item.description}</p>
-                    <a href="${item.link}">View More</a>
+                    <a href="item-template.html?id=${item.id}">View More</a>
                 `;
                 container.appendChild(div);
             });
